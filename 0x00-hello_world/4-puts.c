fdf77f8061720df2afa29039b8c394f924dfc179
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-08 18:43:30 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,9 @@
  *main: Entry point
  *
  *Return: 0 (Success)
- */int main(void)
+ */
+int main(void)
  {
    puts("Programming is like building a multilingual puzzle\n");
-   return(0);
+   return (0);
  }
-
-
-   
