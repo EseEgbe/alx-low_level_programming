eb065d1c3942db1048d2b4b4bbe7eb8f81c24fa5
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-08 19:35:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 /*
  * main - Entry point
  *
+ *Description:using the main funtion
  *this program prints "Programming is like building a multilingual puzzle
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
