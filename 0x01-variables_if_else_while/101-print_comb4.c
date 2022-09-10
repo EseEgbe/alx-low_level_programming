8db1538330ac0636fa9110383b166547c50ba59b
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-10 09:20:48 -0700

    Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: all possible different combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+	int d = 48;
+	int e = 48;
+
+	while (d < 58)
+	{
+		i = 0;
+
+		while (i < 58)
+		{
+			e = 0;
+
+			while (e < 58)
+			{
+				if (d != i && d != e && i != e &&  d < i && i < e)
+				{
+					putchar(i);
+					putchar(d);
+					putchar(e);
+
+					if (i == 56 && d == 55 && e == 57)
+					{
+						break;
+					}
+					putchat(',');
+					putchar(' ');
+				}
+				e++;
+			}
+			i++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
