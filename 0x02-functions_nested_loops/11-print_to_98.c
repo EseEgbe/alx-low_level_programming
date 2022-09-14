c701a320e3f5cae6f761ddb8c74e3ac01aee3b2c
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-13 21:48:02 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,22 @@
 void print_to_98(int n)
 {
 
-	if (n > 98)
+	if (n <= 98)
 	{
-		for (n = 0; n >= 99; n--)
+		while (n <= 98)
 		{
 			_putchar(n);
+			_putchar('.');
+			n++;
 		}
 	}
-	else if (n < 98)
+	else if (n > 98)
 	{
-		for (n = 0; n <= 97; n++)
+		while (n > 98)
 		{
 			_putchar(n);
+			_putchar(',')'
+			n--;
 		}
 	}
 	_putchar('\n');
