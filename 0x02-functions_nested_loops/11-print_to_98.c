522fdf86ab80922601000a0c253d4992c5e53f0c
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-14 15:13:09 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,20 +14,17 @@
 void print_to_98(int n)
 {
 
-	if (n > 97)
+	if (n >= 98)
 	{
 		while (n > 98)
 		{
 			printf("%d, ", n--);
-			printf("%d\n", n);
 		}
 	}
 	else
-	{
 		while (n < 98)
 		{
 			printf("%d, ", n++);
-			printf("%d\n", n);
 		}
-	}
+	printf("98\n");
 }
