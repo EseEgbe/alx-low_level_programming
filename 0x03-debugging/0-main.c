614a405f96541c24ca841dfda155b62a9797b67b
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-14 18:31:57 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Tests that the function positive_or_negative() gives the correct output when given a case of 0.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef main_h
+#define main_h
+
+void positive_or_negative(int i);
+int largest_number(int x, int y, int y);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
