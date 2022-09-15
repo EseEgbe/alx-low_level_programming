43384283c8315efae3f79a56449086ce4641da79
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-14 18:39:48 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Tests that the function positive_or_negative() gives the correct output when given a case of 0.
+ * Description: Tests that the function positive_or_negative()
+ * gives the correct output when given a case of 0.
  *
  * Return: 0 (Success).
  */
@@ -11,9 +12,10 @@
 int main(void)
 {
 	int i;
+
 	i = 0;
 
-	positive_or negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
