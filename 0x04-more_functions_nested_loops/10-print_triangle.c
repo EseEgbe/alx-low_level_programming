58d006f239361dd7c2cbd0dad44d5d869691d6da
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-15 19:42:07 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * Description: prints a triangle
+ * @size: printing of  triangle
+ *
+ * Return: no return value
+ */
+
+void print_triangle(int size)
+
+{
+	int i, n;
+
+	for (i = 0; i < size; i++)
+	{
+		for (n = 1; n < (size - i); n++)
+			_putchar(' ');
+		for (n--; n < size; n++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	}
+	_putchar('\n');
+}
