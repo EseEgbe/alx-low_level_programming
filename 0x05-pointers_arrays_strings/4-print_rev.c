52d349d2313e7882c664f2a44d60ba692d843a30
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-19 16:57:38 -0700

    I can only go one way.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i = 0, len;
 
 	while (s[i++])
 		len++;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = len(s) - 1; i >= 0; i--)
 		_putchar(s[i]);
-	
+
 	_putchar('\n');
 }
