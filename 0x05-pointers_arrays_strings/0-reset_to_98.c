e5b2b5347c376fa4b36a9ac22efc0dbd8f5666f8
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-19 14:50:27 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ *
+ * Description:  takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * Return: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
