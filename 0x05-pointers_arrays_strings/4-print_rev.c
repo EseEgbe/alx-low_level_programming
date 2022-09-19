4c42ec8a7f854b84b7b05ed01d485cc51ff2760d
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-19 16:32:41 -0700

    I can only go one way.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main"
+
+/**
+ * print_rev - entry point
+ * @s: inputed string
+ * Description: prints a string, in reverse, followed by a new line
+ *
+ * Returns: no return value
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	i--;
+
+	_putchar(s[i]);
+
+	_putchar('\n');
+}
