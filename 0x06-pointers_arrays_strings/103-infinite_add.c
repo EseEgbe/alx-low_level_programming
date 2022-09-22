19e689df293dc93e704bc64ea86adae6c44dfb8c
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-21 19:09:19 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,7 +3,7 @@
 
 /**
  * infinite_add - entry point
- * 
+ *
  * Description: adds two numbers
  * @n1: First number.
  * @n2: Second number.
@@ -19,21 +19,17 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + i1) != '\0')
 		i1++;
-
 	while (*(n2 + i2) != '\0')
 		i2++;
-
 	if (i1 >= i2)
 		dg2 = i1;
 	else
 		dg2 = i2;
-
 	if (size_r <= dg2 + 1)
 		return (0);
 	r[dg2 + 1] = '\0';
 	i1--, i2--, size_r--;
 	op1 = *(n1 + i1) - 48, op2 = *(n2 + i2) - 48;
-
 	while (dg2 >= 0)
 	{
 		dg1 = op1 + op2 + sum;
@@ -41,17 +37,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum = dg1 / 10;
 		else
 			sum = 0;
-
 		if (dg1 > 0)
 		*(r + dg2) = (dg1 % 10) + 48;
 		else
 			*(r + dg2) = '0';
-
 		if (i1 > 0)
 			i1--, op1 = *(n1 + i1) - 48;
 		else
 			op1 = 0;
-
 		if (i2 > 0)
 			i2--, op2 = *(n2 + i2) - 48;
 		else
@@ -61,6 +54,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (*(r) == '0')
 		return (r + 1);
 	else
-
 		return (r);
 }
