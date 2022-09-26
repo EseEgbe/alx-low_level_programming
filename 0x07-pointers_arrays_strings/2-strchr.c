22e2c88ba44558a6e35043ffd8e1f7c4d04fbd3a
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-26 10:23:39 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point.
+ *
+ * Description: A function that locates a character in a string.
+ * @s: inputed string.
+ * @c: character to be located.
+ *
+ * Return: pointer to c otherwise, NULL if c not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s[i]);
+		else
+			return ('\0');
+	}
+}
