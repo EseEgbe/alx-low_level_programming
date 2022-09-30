a2ed9c8ae89c9267f0340cacc99f7528c121dc86
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-09-30 15:00:49 -0700

    I do not fear computers. I fear the lack of them.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,19 +2,19 @@
 
 /**
  * _puts - entry point
- * @str: string to be printed
+ * @s: string to be printed
  * Description: prints a string, followed by a new line, to stdout.
  *
  * Returns: no return value.
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
