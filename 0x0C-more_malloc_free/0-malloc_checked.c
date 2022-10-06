fe1f6799f86e0cb64f3ce64d1f497db87adfd819
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-06 04:18:04 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  * Return: pointer to allocated memory.
  */
 
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
 
-	return(ptr);
+	return (ptr);
 }
