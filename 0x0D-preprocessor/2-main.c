0a60814cfb714d129d673f0900f9c47730668d5f
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-11 04:46:37 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#iinclude <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Prints the name of compilation file,
+ *		followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
