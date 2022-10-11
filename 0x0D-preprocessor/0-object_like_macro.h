202d147694be88c05269f610e069b78c92d55f49
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-11 04:36:14 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef object_like_macro_h
+#define object_like_macro_h
+
+/*
+ * File: 0-object_like_macro.h
+ * Description: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
