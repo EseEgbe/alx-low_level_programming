97624f22438418de676dfc76f42ba0ed65f620d6
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-12 10:50:24 -0700

    What is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point.
+ *
+ * Description: Prints a name.
+ * @name: name to print.
+ * @f: pointer to the printing function.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
