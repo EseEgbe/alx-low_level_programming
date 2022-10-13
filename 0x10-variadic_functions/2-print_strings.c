6408690321ee0466f7fe7ba16ac525b27e63dc59
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-10-13 15:11:33 -0700

    Constant vs Variable

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	char *s;
+	unsigned int i;
+
+	va_start(str, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(str, char *);
+
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(str);
+}
