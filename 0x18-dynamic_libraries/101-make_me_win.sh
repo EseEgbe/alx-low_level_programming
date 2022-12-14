d239e3499695f9f3f59df1910fe0cb0817914094
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-12-15 00:18:02 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/EseEgbe/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so
-export LD_PRELOAD=/tmp/gigalib.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
